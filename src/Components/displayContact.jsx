f5a09c15c75dc5578c23d9ad8afbff62fe7f2d1f
ReapingHunter <114232547+ReapingHunter@users.noreply.github.com>
2023-10-03 09:49:56 +0800

    Update displayContact.jsx

--- src/Components/displayContact.jsx
+++ src/Components/displayContact.jsx
@@ -1,24 +1,19 @@
 import React, { useEffect, useState } from "react"
-import UpdateModal from "./updatemodal.jsx";
 import DeleteData from "./deleteData.jsx"
-function showModal() {
-    let ItemUpdate = document.getElementById("updatecontactmodal");
-    let alertredUpdate = document.getElementById("alertdangerupdate");
-    ItemUpdate.style.display = "block";
-    alertredUpdate.style.display = "none";
-    
-}
 
 function displayContact(){
     const [values, setValues] = useState({})
+
     useEffect(() => {
-        fetch('http://192.168.191.12/ContactListBackendPHP/read.php')
+        fetch('http://localhost/ContactListBackendPHP/read.php')
             .then(response=>response.json())
             .then(response => {
                 setValues(response.data)
+
             })
             
     })
+
     return (
         <div>
             <table className="table" id="table">
@@ -37,8 +32,8 @@ function displayContact(){
                     return (
                         <tr key={row + "tr"}>
                             <th scope="row">{values[row].id}</th>
-                            <td>{values[row].firstName}</td>
                             <td>{values[row].lastName}</td>
+                            <td>{values[row].firstName}</td>
                             <td>{values[row].email}</td>
                             <td>{values[row].number}</td>
                             <td>
