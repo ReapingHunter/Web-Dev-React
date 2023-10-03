6f9d5cfa36c20c8a966337da7011baa8d22cca42
ReapingHunter <114232547+ReapingHunter@users.noreply.github.com>
2023-10-03 09:50:18 +0800

    Update createData.jsx

--- src/Components/createData.jsx
+++ src/Components/createData.jsx
@@ -1,6 +1,6 @@
 
 function createData(fname, lname, emailAdd, contactNum){
-    fetch('http://192.168.191.12/ContactListBackendPHP/add.php',{
+    fetch('http://localhost/ContactListBackendPHP/add.php',{
             method: "POST",
             headers: new Headers({
                 "Content-Type": "application/x-www-form-urlencoded"
